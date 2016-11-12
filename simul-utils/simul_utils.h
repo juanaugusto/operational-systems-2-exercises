242f9cefa27bdaef177663b97478c2abfac4822b
Juan Augusto <juan.augusto@ufrj.br>
2016-11-11 22:45:51 -0200

    Modifying header file

--- simul-utils/simul_utils.h
+++ simul-utils/simul_utils.h
@@ -2,6 +2,7 @@
 #define SIMUL_UTILS_H_
 
 char *repl_str(const char *str, const char *from, const char *to);
+void print_error_copy(const char *s1, const char *s2);
 int make_copy(const char *source_f, const char *destination_f);
 
 
